e2300c238773996db672cf30200c3d39d7ceec0c
Jyothisha426 <130735449+Jyothisha426@users.noreply.github.com>
2025-05-08 20:51:30 +0530

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const API = process.env.NEXT_PUBLIC_API_URL;
 
-export const register = (data) => axios.post("http://localhost:5000/api/auth/register", data);
-export const login = (data) => axios.post("http://localhost:5000/api/auth/login", data);
+export const register = (data) => axios.post("https://task-management-backend-q4u7.onrender.com/api/auth/register", data);
+export const login = (data) => axios.post("https://task-management-backend-q4u7.onrender.com/api/auth/login", data);
